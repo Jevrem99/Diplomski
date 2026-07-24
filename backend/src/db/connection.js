137ab04afc048800edbe9508c44519b973a3fb53
vuksi13 <vuksanovicluka7@gmail.com>
2026-07-24 22:41:45 +0200

    env

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,11 +1,11 @@
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'diplomski',
-    password: 'diplomski123',
-    port: 8000,
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'diplomski',
+    password: process.env.DB_PASSWORD || 'postgres',
+    port: process.env.DB_PORT || 5432,
 });
 
 pool.connect()
